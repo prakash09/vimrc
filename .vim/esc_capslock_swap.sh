c09b2f4588bce82f3e8d1a3a2ca2412b1b6314aa
prakash <prakash.gbpec@gmail.com>
2015-02-20 17:48:28 +0530

    corrected code for swapping esc and capslock

--- .vim/esc_capslock_swap.sh
+++ .vim/esc_capslock_swap.sh
@@ -2,15 +2,12 @@
 
 case $1 in
         off)
-                xmodmap -e "clear lock"
-                xmodmap -e "keycode 0x42 = Escape"
+                
+                setxkbmap -option caps:swapescape
                 ;;
         on)
-                xmodmap -e "keycode 0x42 = Caps_Lock"
-                xmodmap -e "add lock = Caps_Lock"
+                
+                setxkbmap -option
                 ;;
-        *)
-                echo "Usage: caps {on|off}, turn the caps lock key on or"
-                echo "off.  When it is off, the key acts as an Escape key."
-                ;;
+       
 esac
